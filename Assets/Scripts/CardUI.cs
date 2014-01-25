bef66befbcb2ca7563ebd240366d78d2e43c7db8
Michael Carter <mcarter@simutronics.com>
2014-01-25 16:14:50 -0600

    Changes.

--- Assets/Scripts/CardUI.cs
+++ Assets/Scripts/CardUI.cs
@@ -5,6 +5,9 @@ public class CardUI : MonoBehaviour
 {
 	public int cardHandIndex;
 	public Renderer glowRenderer;
+	public Material[] cardIconMaterials;
+
+	Slider cardSelectedSlider;
 
 	public bool glow
 	{
@@ -18,11 +21,14 @@ public class CardUI : MonoBehaviour
 	Vector3 initialScale;
 	Vector3 expandedScale;
 
+	public bool selected;
+
 	void Awake()
 	{
 		T = transform;
 		initialScale = T.localScale;
 		expandedScale = 1.1f * initialScale;
+		cardSelectedSlider = GetComponent<Slider>();
 		glowRenderer.gameObject.SetActive(false);
 	}
 
@@ -36,5 +42,27 @@ public class CardUI : MonoBehaviour
 		{
 			T.localScale = initialScale;
 		}
+	}
+
+	public void SetSelected(bool newSelected)
+	{
+		if (newSelected == selected) return;
+
+		cardSelectedSlider.reverseSlide = !newSelected;
+
+		cardSelectedSlider.StartSlide();
+
+		selected = newSelected;
+	}
+
+	public void SetCardIcon(CardIcons icon)
+	{
+		int materialIndex = (int)icon;
+		if (null == cardIconMaterials || materialIndex >= cardIconMaterials.Length)
+		{
+			Debug.LogError("No defined card icon material for card icon type: " + icon);
+			return;
+		}
+		renderer.material = cardIconMaterials[materialIndex];
 	}
 }
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum CardIcons
+{
+	None,
+	Hex,
+	Triangle,
+	Wavy,
+	Square,
+	MAX
+}
+
+public class Deck
+{
+	public CardIcons[] currentHand = new CardIcons[6];
+
+	List<CardIcons> deckList = new List<CardIcons>();
+
+	public int cardsRemaining { get { return deckList.Count; } }
+
+	// TODO: probably need parameters for this
+	public void GenerateDeck()
+	{
+		int[] cardFrequency = new int[5] { 0, 5, 5, 5, 5 };
+
+		for (int i = 0; i < cardFrequency.Length; ++i)
+		{
+			for (int j = 0; j < cardFrequency[i]; ++j)
+			{
+				deckList.Add((CardIcons)i);
+			}
+		}
+
+		ShuffleDeck();
+	}
+
+	void ShuffleDeck()
+	{
+		for (int i = deckList.Count - 1; i >= 0; --i)
+		{
+			CardIcons swap = deckList[i];
+			int swapIndex = Random.Range(0, i);
+			deckList[i] = deckList[swapIndex];
+			deckList[swapIndex] = swap;
+		}
+	}
+
+	// Returns false if there aren't enough cards to fill the hand
+	public bool DrawCardsToHand()
+	{
+		for (int i = 0; i < currentHand.Length; ++i)
+		{
+			if (deckList.Count == 0)
+			{
+				return false;
+			}
+			if (currentHand[i] == CardIcons.None)
+			{
+				currentHand[i] = deckList[0];
+				deckList.RemoveAt(0);
+			}
+		}
+
+		return true;
+	}
+}
