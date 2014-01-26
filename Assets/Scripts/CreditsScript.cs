b61948ed8efec29ecf595bef3ba8c2ec6993f193
Mike <mike@Mikes-MacBook-Pro.local>
2014-01-25 18:34:00 -0600

    Added new assets and altered the BuildSettings for chaning scenes.

---
+++ Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 camPos = mainCamera.transform.position;
+		if (camPos.y < -34) {
+			Application.LoadLevel (0);	 		
+		}
+		mainCamera.transform.Translate (Vector3.down * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			var clickRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hitInfo;
+			Debug.Log(clickRay.ToString());
+			if( Physics.Raycast(clickRay, out hitInfo, 1000f))
+			{
+				var objectHit = hitInfo.collider.gameObject.name;
+
+				if( objectHit == "StartQuest" )
+				{
+					Application.LoadLevel(1);
+				}
+			}
+		}
+	}	
+}
